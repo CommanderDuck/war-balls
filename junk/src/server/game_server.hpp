775956187636316d80d21b72ff916f32529638cb
Igor Slinko <yellowduck@yandex-team.ru>
2015-05-26 19:14:04 +0300

    Removed limitations

--- junk/src/server/game_server.hpp
+++ junk/src/server/game_server.hpp
@@ -27,9 +27,9 @@ struct GameConfig {
         return ((0 < min_players_count && min_players_count < 1000) &&
            (min_players_count < max_states_count && max_players_count < 1000) &&
            (0 < max_states_count && max_states_count < 1000000000)  &&
-           (0 < time_delta && time_delta < 60 * 1000) &&
+           (0 < time_delta) &&
            (0.0 < coin_probability && coin_probability < 1.0) &&
-           (0.0 < field_radius && field_radius < 1000.0) &&
+           (0.0 < field_radius) &&
            (0.0 < max_velocity && max_velocity < field_radius * 0.5) &&
            (0.0 < player_radius && player_radius < field_radius) &&
            (0.0 < coin_radius && coin_radius < field_radius));
